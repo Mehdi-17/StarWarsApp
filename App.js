9ad845f7b29dfe54cb8652e961cb63d6b8afaec2
Mehdi Serra <mehdi.serra57@gmail.com>
2021-07-31 14:39:47 +0200

    intialCommit- Set up app+ home page

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Screens/HomeScreen';
+import StarWarsLogo from './assets/starwars_logo.png';
+
+const Stack = createStackNavigator();
+
+const HeaderLogo = () => {
+  return (
+    <View style={styles.logoStyle}>
+      <Image source={StarWarsLogo} style={{ width: 70, maxHeight: 70, resizeMode: 'contain' }} />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={() => {
+          return {
+            headerTitle: <HeaderLogo />,
+            headerTitleStyle: { flex: 1, alignSelf: 'center' },
+            headerStyle: styles.headerStyle
+          }
+
+        }}>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerStyle: {
+    elevation: 0,
+    height: 70,
+    backgroundColor: 'transparent'
   },
+  logoStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 70,
+    width: '100%'
+  }
 });
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import BackgroundImage from '../assets/homePageBackground.png';
+
+const HomeScreen = () => {
+    const { container, imageBackground } = styles;
+
+    return (
+        <View style={container}>
+            <ImageBackground source={BackgroundImage} style={imageBackground} resizeMode='cover'>
+                <Text>Bonjour</Text>
+            </ImageBackground>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageBackground: {
+        flex: 1,
+        justifyContent: 'center',
+        width: '100%'
+    }
+});
+
+export default HomeScreen;
